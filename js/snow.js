bf6ef258e893876dc665686aed8897d1ef8e03d6
linjinzhong <lin_jin_zhong@outlook.com>
2022-03-08 10:41:36 +0800

    Site updated: 2022-03-08 10:41:36

--- js/snow.js
+++ js/snow.js
@@ -1,7 +1,7 @@
 /*样式一*/
 (function($){
 	$.fn.snow = function(options){
-	var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('&#10052;'),
+	var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('&#165;'), /* 雪花: &#10052 货币: &#165 */
 	documentHeight 	= $(document).height(),
 	documentWidth	= $(document).width(),
 	defaults = {
